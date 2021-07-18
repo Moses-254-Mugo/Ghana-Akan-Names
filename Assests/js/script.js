e08db87651dc1fbf47e145b330523a9f0893b0a3
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-07-18 17:04:23 +0300

    Add event listener to the javascript code

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -3,3 +3,23 @@ const form = document.querySelector('.birthday__input')
 const gender = document.querySelector('input[name="gender"]:checked').value
 const dayControl = document.querySelector('#birthday__day')
 const monthControl = document.querySelector('#birthday__month')
+
+const year = parseInt(document.querySelector('#birthday__year').value)
+const submitBtn = document.querySelector('#birthday__submit')
+
+const birthdayOutput = document.querySelector('.birthday__output')
+const akanName = document.querySelector('#birthday__name')
+
+const day = parseInt(dayControl.value)
+const month = parseInt(monthControl.value)
+
+let _akanName
+let err = ['all fields are required']
+
+dayControl.addEventListener('change', () => {
+    validate()
+})
+
+monthControl.addEventListener('change', () => {
+    validate()
+})
