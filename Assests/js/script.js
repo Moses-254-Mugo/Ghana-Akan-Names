b8fd49d35f667eaa812e49185ef000088ae08187
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-07-18 16:55:38 +0300

    Add a javascript method to get user input

---
+++ Assests/js/script.js
@@ -0,0 +1,5 @@
+// 
+const form = document.querySelector('.birthday__input')
+const gender = document.querySelector('input[name="gender"]:checked').value
+const dayControl = document.querySelector('#birthday__day')
+const monthControl = document.querySelector('#birthday__month')
