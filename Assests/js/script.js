ec626896f5ffb2a02a92ecd9f578a9881a625852
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-07-29 23:59:23 +0300

    Modify html, css, js and README file

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,141 +1,56 @@
-// 
-const form = document.querySelector('.birthday__input')
-const gender = document.querySelector('input[name="gender"]:checked').value
-const dayControl = document.querySelector('#birthday__day')
-const monthControl = document.querySelector('#birthday__month')
 
-const year = parseInt(document.querySelector('#birthday__year').value)
-const submitBtn = document.querySelector('#birthday__submit')
+function checkAkanName(){
+    var day = parseInt(document.getElementById('dob').value);
+    var month = parseInt(document.getElementById('mob').value);
+    var year = parseInt(document.getElementById('yob').value);
+    var gender = document.getElementById('gender').value;
 
-const birthdayOutput = document.querySelector('.birthday__output')
-const akanName = document.querySelector('#birthday__name')
-
-const day = parseInt(dayControl.value)
-const month = parseInt(monthControl.value)
-
-let _akanName
-let err = ['all fields are required']
-
-dayControl.addEventListener('change', () => {
-    validate()
-})
-
-monthControl.addEventListener('change', () => {
-    validate()
-})
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const male_Names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const female_Names = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 
-submitBtn.addEventListener('click', () => {
-    console.log(validate());
+    
+   
+    // validate details
+    if(day <= 0 || day > 31){
 
-    if (validate()) {
-        console.log(`${gender} ${day} ${month} ${year}`);
+        alert("Invalid date!");
+
+    } else if(month <= 0 || month > 12){
+
+        alert("Invalid Month!");
+    } else if(year < 1500 || year > 2021){
+
+        alert("Invalid year!");
+
+    }
+
+    const a = Math.floor((14 - month / 12));
+    const y = year - a;
+    const m = month + 12 * a - 2;
+
+    const d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
+    
+    // validate empty inputs
+    if(document.getElementById("dob").value.length == 0){
+        alert("Day can not be null");
+    }
+    if(document.getElementById("mob").value.length == 0){
+        alert("Month can not be null");
+    }
+    if(document.getElementById("yob").value.length == 0){
+        alert("Incorrect format!!");
+    }
+
+    // Output results
+    if(gender == "male"){
         
-        _akanName = getName()
-        akanName.textContent = _akanName
-
-        birthdayOutput.style.display = 'block'
+        document.getElementById("output").innerHTML = ("You were born on a " + days[d] + " Your Akan Name is " +  male_Names[d]);
     }
-    else {
-        alert(`Invalid: ${err}.`)
+    else if(gender == "female"){
 
-        if (err.indexOf('day') >= 0)
-            dayControl.value = ''
-            
-        if (err.indexOf('month') >= 0)
-            monthControl.value = ''
-
-        err = []
+        document.getElementById("output").innerHTML = ("You were born on a "+ days[d] + " Your Akan Name is " + female_Names[d]);
     }
-})
 
-const validate = () => {
-    if (day <= 0 || day > 31)
-        err.push('day')
-
-    if (month <= 0 || month > 12)
-        err.push('month')
-
-    if (err.length !== 1) {
-        err.shift()
-
-        return false
-    }
-    return true
-}
-
-const getDay = (day, month, year) => {
-    let d                               // Day of the week
-    let dd = day                        // Day of the month
-    let mm = month                      // Month
-    let yy = year % 100                 // Year (89)
-    let cc = (year / 100).toFixed(0)    // Century  (19)
-
-    d = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
-
-    // console.log(d.toFixed(0));
-    return d.toFixed(0)
-}
-
-const getName = () => {
-    console.log(getDay(day, month, year));
-
-    let _name
-    let d = getDay(day, month, year);
-
-    if (gender === 'male') {
-        switch (d * 1) {
-            case 0:
-                _name = 'Kwasi'
-                break;
-            case 1:
-                _name = 'Kwadwo'
-                break;
-            case 2:
-                _name = 'Kwabena'
-                break;
-            case 3:
-                _name = 'Kwaku'
-                break;
-            case 4:
-                _name = 'Yaw'
-                break;
-            case 5:
-                _name = 'Kofi'
-                break;
-            case 6:
-                _name = 'Kwame'
-                break;
-            default:
-                break;
-        }
-    }
-    else {
-        switch (d * 1) {
-            case 0:
-                _name = 'Akousa'
-                break;
-            case 1:
-                _name = 'Adwoa'
-                break;
-            case 2:
-                _name = 'Abenaa'
-                break;
-            case 3:
-                _name = 'Akua'
-                break;
-            case 4:
-                _name = 'Yaa'
-                break;
-            case 5:
-                _name = 'Afua'
-                break;
-            case 6:
-                _name = 'Ama'
-                break;
-            default:
-                break;
-        }
-    }
-    return _name
 }
